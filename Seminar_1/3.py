1fc32ae5297cd87ba0ce875855dc21e5e25d0c4f
Dzek <dzek07@gmail.com>
2022-10-11 19:27:55 +0300

    Программа, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).

--- Seminar_1/3.py
+++ Seminar_1/3.py
@@ -1,11 +1,16 @@
-x = int(input("Введите x: "))
-y = int(input("Введите y: "))
-z = int(input("Введите z: "))
+try:
+    x = int(input("Введите x: "))
+    y = int(input("Введите y: "))
 
-
-a = not (x or y or z) 
-b = not x and not y and not z
-if a == b:
-    print("Вырожение верно")
-else:
-    print("Вырожение не верно")
+    if x > 0 and y > 0:
+        print("x=", x, "y=", y, "-> 1-я координатная четверть")
+    elif x < 0 and y > 0:
+        print("x=", x, "y=", y, "-> 2-я координатная четверть")
+    elif x < 0 and y < 0:
+        print("x=", x, "y=", y, "-> 3-я координатная четверть")
+    elif x > 0 and y < 0:
+        print("x=", x, "y=", y, "-> 4-я координатная четверть")
+    else:
+        print("Вы ввели не коректное значение, введите координаты заново без 0")
+except:
+    print("Введены некоректные данные")
