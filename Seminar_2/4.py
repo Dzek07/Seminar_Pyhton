4f57bb17a556a9d8e2b8a7e0e106bd852fd95140
Dzek <dzek07@gmail.com>
2022-10-17 21:48:08 +0300

    Программа, которая принимает на вход N, и координаты двух точек и находит расстояние между ними в N-мерном пространстве.

---
+++ Seminar_2/4.py
@@ -0,0 +1,22 @@
+from unicodedata import name
+
+
+n = int(input("Введите количество измерений от 1 до бесконечности: "))
+
+a = []
+b = []
+res = []
+
+for i in range(n):
+    print("Введите а", i)
+    temp = int(input())
+    a.append(temp)
+for i in range(n):
+    print("Введите b", i)
+    temp = int(input())
+    b.append(temp)
+for i in range(n):
+    temp = (a[i] - b[i]) ** 2
+    res.append(temp)
+
+print("Расстояние = ", sum(res)**0.5)
